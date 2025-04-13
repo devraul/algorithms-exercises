25e22b708823648c06d4b48ff61ce2bca7e68100
Raul Melo <melo.raulf@gmail.com>
2025-04-13 10:39:10 +0200

    implement bubble sort

--- specs/bubble-sort/bubble-sort.test.ts
+++ specs/bubble-sort/bubble-sort.test.ts
@@ -10,14 +10,33 @@
   index's. Continue looping through until all values are in ascending order
 */
 
-function bubbleSort(nums) {
-  // code goes here
-  return null;
+function bubbleSort(nums: number[]) {
+  const copy = [...nums];
+  let hasBeenSwaped: boolean = false;
+
+  do {
+    hasBeenSwaped = false;
+
+    for (let currentIndex = 0; currentIndex < copy.length; currentIndex++) {
+      const nextIndex = currentIndex + 1;
+      const currentElement = copy[currentIndex];
+      const nextElement = copy[nextIndex];
+
+      if (currentElement > nextElement) {
+        copy[currentIndex] = nextElement;
+        copy[nextIndex] = currentElement;
+
+        hasBeenSwaped = true;
+      }
+    }
+  } while (hasBeenSwaped);
+
+  return copy;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
